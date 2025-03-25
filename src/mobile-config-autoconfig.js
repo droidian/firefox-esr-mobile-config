7543ac33644f34d88ac15a73237ad8a31cfb7718
Danny Colin <contact@dannycolin.com>
2025-03-25 18:08:14 -0400

    Add User-Agent spoofing capability
    Some websites aren't working well with our default user-agent.
    This patch adds a function that let us spoof a different
    user-agent based on an URL regex.
    
    Part-of: https://gitlab.postmarketos.org/postmarketOS/mobile-config-firefox/-/merge_requests/77

--- src/mobile-config-autoconfig.js
+++ src/mobile-config-autoconfig.js
@@ -272,29 +272,72 @@ function css_files_update() {
 }
 
 /**
- * Builds a user-agent as similar to the default as possible, but with "Mobile"
- * inserted into the platforms section.
- *
- * @returns {string}
+ * Set default user agent and override it for specific websites
  */
-function build_user_agent() {
-    var appinfo = Services.appinfo;
-    var vendor = appinfo.vendor || "Mozilla";
-    var os = appinfo.OS || "Linux";
-    var version = get_firefox_version() + ".0";
-    var name = appinfo.name || "Firefox";
-    var arch = (appinfo.XPCOMABI && appinfo.XPCOMABI.includes("-"))
-        ? appinfo.XPCOMABI.split("-")[0]
-        : "aarch64";
+function set_user_agent() {
+    const APP_VERSION = `${AppConstants.MOZ_APP_VERSION.split(".")[0]}.0`;
 
-    return `${vendor}/5.0 (X11; ${os} ${arch}; Mobile; rv:${version}) Gecko/20100101 ${name}/${version}`;
+    // We added `Mobile;` to the default Firefox Desktop user agent
+    const UA_FIREFOX_DESKTOP = `Mozilla/5.0 (X11; Linux x86_64; Mobile; rv:${APP_VERSION}) Gecko/20100101 Firefox/${APP_VERSION}`;
+    const UA_FIREFOX_ANDROID = `Mozilla/5.0 (Android 15; Mobile; rv:${APP_VERSION}) Gecko/${APP_VERSION} Firefox/${APP_VERSION}`;
+    // Google version API: https://developer.chrome.com/docs/web-platform/versionhistory/reference
+    const UA_CHROME_ANDROID = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.135 Mobile Safari/537.36";
+    const UA_CHROME_CHROMEOS = "Mozilla/5.0 (X11; CrOS aarch64 15329.44.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.137 Safari/537.36";
+
+    // Set default user agent
+    defaultPref('general.useragent.override', UA_FIREFOX_DESKTOP);
+
+    const userAgentRules = {
+        // Google
+        // FIX: Google login not trusting the browser
+        "^https?://accounts.google.com($|/)": UA_FIREFOX_DESKTOP,
+        // FIX: Google Search showing up as the old layout
+        "^https?://(wwww.)?google.com($|/)": UA_FIREFOX_ANDROID,
+        // FIX: ???
+        "^https?://drive.google.com($|/)": UA_CHROME_ANDROID,
+        // FIX: Google Maps search bar not being interactive
+        "^https?://(www.)?google.com/maps($|/)": UA_FIREFOX_DESKTOP,
+        "^https?://maps.google.com($|/)": UA_FIREFOX_DESKTOP,
+
+        // Firefox
+        // FIX: Sync login not completing
+        "^https?://accounts.firefox.com($|/)": UA_FIREFOX_DESKTOP,
+
+        // Mozilla
+        // FIX: AMO thinks we want the android extensions
+        "^https?://addons.mozilla.org($|/)": UA_FIREFOX_DESKTOP,
+
+        // Youtube
+        // FIX: YouTube fullscreen acting weird
+        "^https?://(m.)?youtube.com($|/)": UA_FIREFOX_ANDROID,
+
+        // Netflix
+        // FIX: Netflix refusing to playback even if EME is working
+        "^https?://(www.)?netflix.com($|/)": UA_CHROME_CHROMEOS,
+    };
+
+    const requestObserver = {
+        observe: function(subject, topic, data) {
+            if (topic == "http-on-modify-request") {
+                const httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
+                const uri = httpChannel.URI.spec;
+
+                for (const [urlPattern, userAgentRule] of Object.entries(userAgentRules)) {
+                    if (new RegExp(urlPattern).test(uri)) {
+                        httpChannel.setRequestHeader("User-Agent", userAgentRule, false);
+                        break;
+                    }
+                }
+            }
+        }
+    };
+
+    const observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+    observerService.addObserver(requestObserver, "http-on-modify-request", false);
 }
 
 function set_default_prefs() {
     log("Setting default preferences");
-
-    var user_agent = build_user_agent();
-    defaultPref('general.useragent.override', user_agent);
 
     // Do not suggest facebook, ebay, reddit etc. in the urlbar. Same as
     // Settings -> Privacy & Security -> Address Bar -> Shortcuts. As
@@ -371,6 +414,9 @@ function set_default_prefs() {
 function main() {
     log("Running mobile-config-autoconfig.js");
     css_files_update();
+
+    // Set default user agent and override it for specific websites
+    set_user_agent();
 
     // Restart Firefox immediately if one of the files got updated
     if (g_updated == true)
